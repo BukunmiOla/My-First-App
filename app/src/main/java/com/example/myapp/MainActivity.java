3bd68aa02c92b0822da7a4e3bea339787b72f7ae
BukunmiOla <ebukunmiola@gmail.com>
2020-03-20 19:02:50 +0100

    added Extras

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,28 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-   private Button _Ok;
+    String name, dept,institution, info;
+    TextView readyText;
+    EditText nameText, deptText, institutionText;
+    private Button _Ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        nameText= findViewById(R.id.name_text);
+        deptText= findViewById(R.id.dept_text);
+        institutionText=findViewById(R.id.institution_text);
         _Ok = findViewById(R.id.ok_button);
+
         _Ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                name = nameText.getText().toString();
+                dept = deptText.getText().toString();
+                institution = institutionText.getText().toString();
+                info= "Hi, "+ name + "\nWelcome to " +dept+" Department in " + institution;
                 Welcome();
             }
         });
+
+
 
     }
 
     private void Welcome(){
         Intent welcome= new Intent(MainActivity.this, NextActivity.class);
+        welcome.putExtra("message",info);
         startActivity(welcome);
     }
 
--- app/src/main/java/com/example/myapp/NextActivity.java
+++ app/src/main/java/com/example/myapp/NextActivity.java
@@ -3,12 +3,23 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class NextActivity extends AppCompatActivity {
-
+    TextView readyText, sleepText, runText;
+    EditText infoText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        infoText = findViewById(R.id.info);
+
+        Intent Here = getIntent();
+        String info= Here.getExtras().getString("message");
+        infoText.setText(info);
+
     }
 }
